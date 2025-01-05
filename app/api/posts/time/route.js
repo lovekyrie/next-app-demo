3086c5d907d785c969b92ebcab5026ba2cfb36f1
lovekyrie <fengdurant@163.com>
2025-01-05 08:16:45 +0800

    how to write middleware

--- app/api/posts/time/route.js
+++
@@ -1,4 +0,0 @@
-export async function GET() {
-  console.log('GET /api/time')
-  return Response.json({ data: new Date().toLocaleTimeString() })
-}
---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async redirects() {
+    return [{ source: '/blog/yayu', destination: '/blog/yayu_redirects', permanent: true }]
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [{ source: '/blog/yayu', destination: '/blog/yayu_beforeFiles' }],
+      afterFiles: [{ source: '/blog/yayu', destination: '/blog/yayu_afterFiles' }],
+      fallback: [{ source: '/blog/yayu', destination: '/blog/yayu_fallback' }],
+    }
+  },
+}
